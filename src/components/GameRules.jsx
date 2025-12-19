f6de96cb7a991e73e7e7662f1393e561786cfeb8
flaviare1s <flaviarster@gmail.com>
2025-12-19 08:28:34 -0300

    Ajusta estilos

--- src/components/GameRules.jsx
+++ src/components/GameRules.jsx
@@ -6,12 +6,14 @@ export const GameRules = () => {
     <div className='rules-wrapper'>
         <div className='rules'>
           <h2>Game Rules</h2>
-              <p>Try to guess the name of the movie.</p>
-              <p>I'll give you five tips.</p>
-              <p>With every wrong guess you lose a life.</p>
-              <p>Be careful, you only have ten lives.</p>
-              <p>You earn a higher score when you use fewer hints.</p>
-              <p>Good luck and let's get started!</p>
+              <ul>
+                <li>Try to guess the name of the movie.</li>
+                <li>I'll give you five tips.</li>
+                <li>With every wrong guess you lose a life.</li>
+                <li>Be careful, you only have ten lives.</li>
+                <li>You earn a higher score when you use fewer hints.</li>
+                <li>Good luck and let's get started!</li>
+              </ul>
         </div>
     </div>
   )
