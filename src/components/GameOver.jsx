2be5c5bc8bd62088470148a4f955b61ed9ff2022
flaviare1s <flaviarster@gmail.com>
2025-12-19 09:59:24 -0300

    Ajusta estilos da tela de game over

--- src/components/GameOver.jsx
+++ src/components/GameOver.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable react/prop-types */
 import './GameOver.css'
 
 export function GameOver({ onRestart }) {
   return (
-    <div>
+    <div className="game-over">
       <h1>Game Over!</h1>
       <p>You lost all your lives!</p>
       <button onClick={onRestart}>Restart</button>
