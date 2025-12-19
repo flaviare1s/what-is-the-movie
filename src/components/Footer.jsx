f58523f6efcb51cbfc2e9aa871d48cfc44874715
flaviare1s <flaviarster@gmail.com>
2025-12-19 16:10:01 -0300

    Adiciona footer e ajusta estilos

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import "./Footer.css"
+
+export const Footer = () => {
+  return (
+    <footer>
+      <p>Developed by <a href="https://github.com/flaviare1s" target="_blank" rel="noopener noreferrer">Flávia Reis</a></p>
+    </footer>
+  )
+}
